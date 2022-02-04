3bb87b219b8c9d8965772f58a1436a67a1b1e0fb
ChrisCooper0 <christopher.cooper@mail.com>
2022-02-04 18:50:51 +0000

    init

---
+++ start/server/index.js
@@ -0,0 +1,56 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { mainCards, animals } = require("./db");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  type MainCard {
+    id: ID!
+    title: String!
+    image: String!
+  }
+
+  type Animal {
+    id: ID!
+    image: String!
+    title: String!
+    rating: Float
+    price: String!
+    description: [String!]!
+    stock: Int!
+    onSale: Boolean
+    slug: String!
+  }
+
+  type Query {
+    mainCards: [MainCard]
+    animals: [Animal!]
+    animal(slug: String!): Animal
+  }
+`;
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    mainCards: () => mainCards,
+    animals: () => animals,
+    // Query for a specific animal
+    animal: (_, { slug }, __, ___) => {
+      let animal = animals.find((animal) => {
+        return animal.slug === slug;
+      });
+      return animal;
+    },
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
