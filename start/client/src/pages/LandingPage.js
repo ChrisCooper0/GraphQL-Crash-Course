3046de6894bf8f2e2a98077a7680c4d9c5736b00
ChrisCooper0 <christopher.cooper@mail.com>
2022-02-05 14:38:06 +0000

    add client

--- start/client/src/pages/LandingPage.js
+++ start/client/src/pages/LandingPage.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import MainHero from "../components/MainHero/MainHero"
-import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
-import CardDisplay from "../components/CardDisplay/CardDisplay"
+import React from "react";
+import MainHero from "../components/MainHero/MainHero";
+import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay";
+import CardDisplay from "../components/CardDisplay/CardDisplay";
+import { useQuery, gql } from "@apollo/client";
+
+const ANIMALS_QUERY = gql`
+  {
+    animals {
+      id
+      image
+      title
+      price
+      stock
+      slug
+    }
+  }
+`;
 
 function LandingPage() {
+  const { loading, error, data } = useQuery(ANIMALS_QUERY);
 
-    return (
-        <div>
-            <MainHero />
-            <CategoryDisplay />
-            <CardDisplay animals={[]}/>
-        </div>
-    )
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+
+  return (
+    <div>
+      <MainHero />
+      <CategoryDisplay />
+      <CardDisplay animals={data.animals} />
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
